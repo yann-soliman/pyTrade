cef1a921c654271317d6156959f3e90e354234d6
Yann Soliman <yann.soliman@gmail.com>
2017-11-30 23:21:19 +0100

    Test email

--- alert/EmailAlert.py
+++ alert/EmailAlert.py
@@ -1,17 +1,22 @@
 import os
-import requests
+
+from sendgrid import sendgrid, Email
+from sendgrid.helpers.mail import Content, Mail
 
 
 class EmailAlert:
     def __init__(self):
-        self.domain = os.environ.get('MAILGUN_DOMAIN')
-        self.api_key = os.environ.get('MAILGUN_API_KEY')
+        self.api_key = "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"
 
     def send_simple_message(self):
-        return requests.post(
-            "https://api.mailgun.net/v3/" + self.domain + "/messages",
-            auth=("api", self.api_key),
-            data={"from": "Excited User <mailgun@" + self.domain + ">",
-                  "to": ["yann.soliman@gmail.com", "yann.soliman@gmail.com"],
-                  "subject": "Hello",
-                  "text": "Testing some Mailgun awesomness!"})
+        sg = sendgrid.SendGridAPIClient(apikey=self.api_key)
+        from_email = Email("test@example.com")
+        subject = "Hello World from the SendGrid Python Library!"
+        to_email = Email("yann.soliman@gmail.com")
+        content = Content("text/plain", "Hello, Email!")
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        print(os.environ.get('SENDGRID_API_KEY'))
