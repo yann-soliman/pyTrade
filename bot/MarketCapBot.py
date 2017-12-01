9610dabc159ae1d336756a41a5273ca5055c7e07
yann-soliman <22856901+yann-soliman@users.noreply.github.com>
2017-12-01 15:15:47 +0100

    Ajout de matt

--- bot/MarketCapBot.py
+++ bot/MarketCapBot.py
@@ -5,13 +5,14 @@ from connector.CoinMarketCapConnector import CoinMarketCapConnector
 
 
 class MarketCapBot:
-    THRESHOLD = 1
+    THRESHOLD = 0.8
 
     def __init__(self):
         self.alert = EmailAlert()
         self.coinmarket_cap_connector = CoinMarketCapConnector()
         self.me = "yann.soliman@gmail.com"
         self.mf = "tom_lyo@hotmail.fr"
+        self.matt = "langlois.matt@gmail.com"
 
     def run(self):
         previous_cap = 99999999999
@@ -22,6 +23,7 @@ class MarketCapBot:
                 message = "ALERTE, coinmarket cap avant : " + str(previous_cap) + " et maintenant : " + str(cap)
                 self.alert.send_message(self.me, message)
                 self.alert.send_message(self.mf, message)
+                self.alert.send_message(self.matt, message)
                 print("ALERTE, coinmarket cap avant : " + str(previous_cap) + " et maintenant : " + str(cap))
             previous_cap = cap
             sleep(10)
